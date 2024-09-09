6b827cb70189a16b3f62f75548ec9472a1fc426e
Maxim Koshkin <humblerat@mail.ru>
2024-09-09 15:11:28 +0700

    migrations: added access_tokens table, updated users table

---
+++ database/migrations/2024_09_08_064735_add_columns_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('email')->nullable()->change();
+            $table->string('password')->nullable()->change();
+            $table->string('username')->unique()->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('second_name')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('email')->nullable(false)->change();
+            $table->string('password')->nullable(false)->change();
+            $table->dropColumn('username');
+            $table->dropColumn('first_name');
+            $table->dropColumn('second_name');
+        });
+    }
+};
