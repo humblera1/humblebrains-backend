d9906e21e685b2d2857afdeedc58ddb465a265ee
Maxim Koshkin <humblerat@mail.ru>
2024-09-13 19:51:09 +0700

    added 'login' and 'register' gates to prevent requests from already authenticated user

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Services\Api\AuthService;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('login', function(?User $user = null) {
+            if (app(AuthService::class)->isCurrentUserFullFledged()) {
+                return Response::deny('You\'re already logged in!');
+            }
+
+            return Response::allow();
+        });
+
+        Gate::define('register', function(?User $user = null) {
+            if (app(AuthService::class)->isCurrentUserFullFledged()) {
+                return Response::deny('You\'re already aboard!');
+            }
+
+            return Response::allow();
+        });
     }
 }
