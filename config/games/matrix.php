a0ffd38a841b6b257afc9171ad90893ae0c9cb27
Maxim Koshkin <humblerat@mail.ru>
2024-09-24 13:48:20 +0700

    added config files

---
+++ config/games/matrix.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'name' => 'matrix',
+    'label' => [
+        'ru' => 'Матрица',
+        'en' => 'Matrix',
+    ],
+    'description' => [
+        'ru' => 'Описание игры матрица',
+        'en' => 'Matrix description',
+    ],
+//    'main_image' => '',
+//    'thumbnail_image' => '',
+//    'icon_image' => '',
+    'max_level' => 30,
+];
---
+++ config/properties/matrix.php
@@ -0,0 +1,83 @@
+<?php
+
+return [
+    [
+        'name' => 'square_side',
+        'label' => [
+            'en' => 'Square side',
+            'ru' => 'Сторона квадрата',
+        ],
+        'description' => [
+            'en' => 'Square side of the game',
+            'ru' => 'Сторона квадрата',
+        ],
+    ],
+    [
+        'name' => 'cells_amount_to_reproduce',
+        'label' => [
+            'en' => 'Number of cells to reproduce',
+            'ru' => 'Количество ячеек для запоминания',
+        ],
+        'description' => [
+            'en' => 'Number of cells that need to be remembered and then reproduced',
+            'ru' => 'Количество ячеек, которые необходимо запомнить и воспроизвести',
+        ],
+    ],
+    [
+        'name' => 'colors_amount',
+        'label' => [
+            'en' => 'Number of colors',
+            'ru' => 'Количество цветов',
+        ],
+        'description' => [
+            'en' => 'Number of different colors on the playing field',
+            'ru' => 'Количество различных цветов на игровом поле',
+        ],
+    ],
+    [
+        'name' => 'correct_answers_before_promotion',
+        'label' => [
+            'en' => 'Number of correct answers before leveling up',
+            'ru' => 'Количество правильных ответов до повышения уровня',
+        ],
+        'description' => [
+            'en' => 'After this number of correct answers given in a row, the level will be increased.',
+            'ru' => 'После данного количества правильных ответов, данных подряд, уровень будет повышен',
+        ],
+    ],
+    [
+        'name' => 'incorrect_answers_before_demotion',
+        'label' => [
+            'en' => 'Number of incorrect answers before level decreases',
+            'ru' => 'Количество неправильных ответов до понижения уровня',
+        ],
+        'description' => [
+            'en' => 'After this number of incorrect answers given in a row, the level will be reduced to the previous one',
+            'ru' => 'После данного количества неправильных ответов, данных подряд, уровень будет понижен до предыдущего',
+        ],
+    ],
+    [
+        'name' => 'rotation_iterations',
+        'label' => [
+            'en' => 'Number of rotations',
+            'ru' => 'Количество поворотов',
+        ],
+        'description' => [
+            'en' => 'The number of rotations the playing field will make',
+            'ru' => 'Количество поворотов, которое совершает игровое поле',
+        ],
+    ],
+    [
+        'name' => 'has_order',
+        'label' => [
+            'en' => 'Checking the order',
+            'ru' => 'Проверка порядка',
+        ],
+        'description' => [
+            'en' => 'Should the game additionally check that the order in which cells are opened matches the order in which they are shown to the user',
+            'ru' => 'Следует ли дополнительно проверять соответствие порядка открытия ячеек порядку их показа пользователю',
+        ],
+    ],
+
+    // todo: время на запоминание и время на ответ
+];
