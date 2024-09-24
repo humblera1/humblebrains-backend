a0531d215c0e07dc0fc2e3f49fb79deee1c4e381
Maxim Koshkin <humblerat@mail.ru>
2024-09-24 17:48:16 +0700

    updated command (works only with arg for now)

--- config/games/matrix.php
+++ config/games/matrix.php
@@ -10,8 +10,27 @@ return [
         'ru' => 'Описание игры матрица',
         'en' => 'Matrix description',
     ],
-//    'main_image' => '',
-//    'thumbnail_image' => '',
-//    'icon_image' => '',
-    'max_level' => 30,
+    //    'main_image' => '',
+    //    'thumbnail_image' => '',
+    //    'icon_image'
+    'levels' => [
+        1 => [
+            'square_side' => 3,
+            'cells_amount_to_reproduce' => 1,
+            'colors_amount' => 1,
+            'correct_answers_before_promotion' => 1,
+            'incorrect_answers_before_demotion' => 1,
+            'rotation_iterations' => 0,
+            'has_order' => 'false',
+        ],
+        2 => [
+            'square_side' => 3,
+            'cells_amount_to_reproduce' => 2,
+            'colors_amount' => 1,
+            'correct_answers_before_promotion' => 2,
+            'incorrect_answers_before_demotion' => 3,
+            'rotation_iterations' => 0,
+            'has_order' => 'false',
+        ]
+    ]
 ];
